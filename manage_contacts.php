402f5af4a848bee31afd11f66fd10648e4941815
sipubaruactg <sipubaruactg100@gmail.com>
2026-06-25 04:29:20 +0600

    Update manage_contacts.php

--- manage_contacts.php
+++ manage_contacts.php
@@ -51,6 +51,7 @@ $contacts = $conn->query("SELECT * FROM customers ORDER BY id DESC");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google" content="notranslate">
     <title>Contact Management</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>body { background-color: #030712; }</style>
